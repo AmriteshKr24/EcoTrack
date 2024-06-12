e19f1d4f78e21d7079e360ecd67eb25fd19fdb37
Arunava Chakrabarty <137680973+CapAru@users.noreply.github.com>
2024-06-12 17:19:07 +0530

    Configured Source files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js}"],
+  content: ["./public/*.{html,js}", "./views/pages/*.{html, js, ejs}", "./views/partials/*.{html, js, ejs}"],
   theme: {
     extend: {},
   },
