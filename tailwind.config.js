aa972b4eabc712a286d337c200aa40954b1f216f
MANIK <2022itb100.manik@students.iiests.ac.in>
2024-06-13 14:45:37 +0530

    stylesheet path

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,13 @@ module.exports = {
   content: ["./views/pages/*.{html,js,ejs}", "./views/partials/*.{html,js,ejs}", "./public/*.{html,js,ejs}"],
   theme: {
     extend: {
-      fontFamily:{
-        indie: ["Indie Flower","cursive","sans-serif"],
-        julius: ["Julius Sans One","sans-serif"],
+      fontFamily: {
+        indie: ["Indie Flower", "cursive", "sans-serif"],
+        julius: ["Julius Sans One", "sans-serif"],
       }
     },
   },
   plugins: [],
 }
+
+
