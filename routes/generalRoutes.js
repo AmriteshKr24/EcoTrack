6c73149b34678849de9af58704fd87a3a641b3bd
MANIK <2022itb100.manik@students.iiests.ac.in>
2024-06-29 09:59:42 +0530

    leaderboard

--- routes/generalRoutes.js
+++ routes/generalRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -6,8 +7,9 @@ router.get('/home', (req, res) => {
     res.render('pages/home');
 });
 
-router.get('/leaderboard', (req, res) => {
-    res.render('pages/leaderboard', { title: 'Leaderboard' });
+router.get('/leaderboard', async (req, res) => {
+    const users = await User.find({});
+    res.render('pages/leaderboard', { title: 'Leaderboard', users });
 });
 
 module.exports = router;
